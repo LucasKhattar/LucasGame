ac8bc82f1d484cd6b6e68dc3fbaf8eaeff8ea1e0
tallyhawley <league@WTS5.attlocal.net>
2017-04-06 20:04:02 -0700

    Part 2
    LOLOL

--- src/LucasGamePanel.java
+++ src/LucasGamePanel.java
@@ -1,7 +1,26 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LucasGamePanel {
-	JPanel panel;
-LucasGamePanel(){
-	
-}
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class LucasGamePanel extends JPanel implements ActionListener{
+
+	Timer timer;
+
+	LucasGamePanel() {
+		timer = new Timer(1000 / 60, this);
+	}
+	void startGame(){
+		timer.start();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Lucas");
+		System.out.println("is");
+		System.out.println("awesome");
+		System.out.println("!");
+	}
 }
--- src/WhosThatPokemon.java
+++ src/WhosThatPokemon.java
@@ -2,18 +2,29 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class WhosThatPokemon {
-	WhosThatPokemon(){
-		
-	}
-		public static void main(String[] args) {
-			
-		}
-		JFrame frame;
-		void setup(){
-			frame = new JFrame();
-			panel = new JPanel();
-			frame.add(panel);
-		}
+	WhosThatPokemon() {
+		frame = new JFrame();
+		panel = new LucasGamePanel();
+		setup();
 	}
 
-//Pokemon images http://pokemon.wikia.com/wiki/Category:Generation_I_Pok%C3%A9mon
+	JFrame frame;
+	LucasGamePanel panel;
+	final int width = 800;
+	final int height = 500;
+
+	public static void main(String[] args) {
+		WhosThatPokemon pokemon = new WhosThatPokemon();
+	}
+
+	void setup() {
+		frame.setVisible(true);
+		frame.setSize(width, height);
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		panel.startGame();
+	}
+}
+
+// Pokemon images
+// http://pokemon.wikia.com/wiki/Category:Generation_I_Pok%C3%A9mon
